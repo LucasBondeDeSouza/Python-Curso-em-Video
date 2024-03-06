a1a986feae7ced6dc809f7e14d110d6011c4a95d
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-03-06 14:13:59 -0300

    Cores no Terminal

---
+++ 07-Cores no Terminal/exemplo01.py
@@ -0,0 +1,9 @@
+# print('\033[31mOlá, Mundo!') -> Letra Vermleho
+
+# print('\033[31;43mOlá, Mundo!') -> Fundo Amarelo com Letra Vermelha
+
+# print('\033[1;31;43mOlá, Mundo!') -> Fundo Amarelo com Letra Vermelha e Negrito
+
+# print('\033[1;31;43mOlá, Mundo!\033[m') -> Fundo Amarelo até o final da frase com Letra Vermelha e Negrito
+
+print('\033[7;33;44mOlá, Mundo!\033[m')
